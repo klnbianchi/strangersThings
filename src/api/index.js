b359c952a3e2df1faeadb46622d13e9d6405c11b
Kendra <klnbianchi@gmail.com>
2021-09-14 14:18:13 -0700

    Post component: rendered posts

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const BASE = 'https://strangers-things.herokuapp.com/api/2106-CPU-RM-WEB-PT/'
 
-export async function getAllPosts() {
+export async function fetchAllPosts() {
   try {
-    const { data } = await axios.get(`${ BASE }/posts`);
-    return data;
+    const {data:{data}} = await axios.get(`${ BASE }/posts`);
+    return data.posts
   } catch (error) {
     throw error;
   }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchAllPosts } from '../api';
 
 const Post = (props) => {
-  return (
-    <div>
-      <h1>hello world!</h1>
-      <h1>hello world!</h1>
-    </div>
-  )
+    const [allPosts, setAllPosts] = useState([]);
+
+    useEffect(async () => {
+        const posts = await fetchAllPosts();
+        setAllPosts(posts)
+    }, []);
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            {
+                allPosts.length
+                    ? allPosts.map(e => {
+                        return (
+                            <div key={e._id}>
+                                <h3>{e.title}</h3>
+                                <p>{e.description}</p>
+                            </div>
+                        )
+                    })
+                    : null
+            }
+        </div>
+    )
 }
 
 export default Post;
