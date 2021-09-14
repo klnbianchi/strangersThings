9985dc81cdeb4569a8696a819c7188f6b29dc088
Gabrielle La Madrid <galamadrid917@gmail.com>
2021-09-13 21:27:51 -0700

    Updated Post.2

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 const Post = (props) => {
+  const [posts, setPosts] = useState([]);
+  console.log('posts:', posts);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const resp = await fetch ('https://jsonplace-univclone.herokuapp.com/posts');
+      const data = await resp.json();
+      console.log('data: ', data);
+      setPosts(data);
+    }
+
+    fetchPosts();
+
+  }, [])
+
   return (
-    <div>
-      <h1>hello world!</h1>
-      <h1>hello world!</h1>
+    <div id="App">
+      <h1>Stranger Things</h1>
+      {
+        posts.map(post => <div>key={post.id}
+        {post.title}</div>)
+      }
+      <Header />
     </div>
-  )
-}
+  );
+};
 
 export default Post;
