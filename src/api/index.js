8650cd86446817cfeb1166e9b949947f45d8e0f6
Kendra <klnbianchi@gmail.com>
2021-09-13 20:51:03 -0700

    added base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const BASE = 'https://jsonplace-univclone.herokuapp.com'
+const BASE = 'https://strangers-things.herokuapp.com/api/2106-CPU-RM-WEB-PT/'
 
-export async function getUsers() {
+export async function getAllPosts() {
   try {
-    const { data } = await axios.get(`${ BASE }/users`);
+    const { data } = await axios.get(`${ BASE }/posts`);
     return data;
   } catch (error) {
     throw error;
