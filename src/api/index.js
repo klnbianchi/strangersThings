f7fb0a14d85f0f8fd4982a2519a8b7a0c405e49c
Kendra <klnbianchi@gmail.com>
2021-09-15 20:44:03 -0700

    added the login component and updated api and auth

--- src/api/index.js
+++ src/api/index.js
@@ -11,3 +11,28 @@ export async function fetchAllPosts() {
   }
 }
 
+export async function loginUser(username, password){
+  try {
+    const {data} = await axios.post(`${ BASE }/users/login`,
+    {
+      username: username,
+      password: password,
+    });
+    return data
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function registerUser(username, password){
+  try {
+    const {data} = await axios.post(`${ BASE }/users/register`,
+    {
+      username: username,
+      password: password,
+    });
+    return data
+  } catch (error) {
+    throw error;
+  }
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { loginUser } from '../api'
+import { storeToken } from '../auth';
 
-const Login = (props) => {
-  return (
-    <div>
-      <h1>login</h1>
-    </div>
-  )
+const Login = ({ setIsLoggedIn }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className="login">
+            <h2>Log-in to your account</h2>
+            <form
+                className="login-form"
+                onSubmit={async (e) => {
+                    e.preventDefault();
+
+                    try {
+                        const results = await loginUser(username, password);
+                        storeToken(results.data.token)
+                        setIsLoggedIn(true);
+                        setUsername('');
+                        setPassword('');
+                    } catch (err) {
+                        console.log(err)
+                    } finally {
+
+                    }
+                }}>
+
+                <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Username" />
+
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password" />
+
+                <button>Login</button>
+            </form>
+
+            <p>Don't have an account? <a href="./">Sign Up</a></p>
+        </div>
+    )
 }
 
 export default Login;
