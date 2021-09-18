26a7edcde3f53024cc44841a592fc3f3e11e58b5
Kendra <klnbianchi@gmail.com>
2021-09-18 14:04:50 -0700

    added single post component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { default as Post } from './Post';
+export { default as SinglePost } from './SinglePost';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as CreatePost } from './CreatePost';
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import { async } from 'q';
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import {fetchAllPosts, fetchUserData} from './api'
+import {getToken} from './auth'
 
 import {
   BrowserRouter as Router,
@@ -10,18 +12,39 @@ import {
   Redirect
 } from 'react-router-dom'
 
-import { Post, Login, Register } from "./components"
+import { 
+  Post, 
+  Login, 
+  Register 
+} from "./components"
 
 const App = () => {
   const [isLoggedIn, setIsLoggedin] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [userPosts, setUserPosts] = useState([]);
+  const [userName, setUserName] = useState('');
+  const [allPosts, setAllPosts] = useState([]);
+
+  useEffect(async () => {
+    const posts = await fetchAllPosts();
+    setAllPosts(posts)
+  }, []);
+
+  useEffect(async () => {
+    const userToken = getToken();
+    const userInfo = await fetchUserData(userToken);
+    setUserPosts(userInfo.posts);
+    setUserName(userInfo.username)
+  }, []);
 
   return (
     <div id="App">
       <h1>Hello, World!!!</h1>
       <Login />
       <Register />
-      <Post />
+      <Post
+       allPosts={allPosts}
+       />
     </div>
   );
 };
